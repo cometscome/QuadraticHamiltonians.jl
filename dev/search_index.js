581842984d71da86251786417d1613df71ba440d
Documenter.jl <documenter@juliadocs.github.io>
2025-10-14 06:21:42 +0000

    build based on 5b3d6d5

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QuadraticHamiltonians","category":"page"},{"location":"#QuadraticHamiltonians","page":"Home","title":"QuadraticHamiltonians","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuadraticHamiltonians.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QuadraticHamiltonians]","category":"page"}]
+[{"location":"#QuadraticHamiltonians","page":"Home","title":"QuadraticHamiltonians","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuadraticHamiltonians.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
 }
