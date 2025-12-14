aae316db618559d0bbcf40d38d687ee7aa388012
Documenter.jl <documenter@juliadocs.github.io>
2025-12-14 11:45:18 +0000

    build based on 4b25d86

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#QuadraticHamiltonians","page":"Home","title":"QuadraticHamiltonians","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for QuadraticHamiltonians.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#QuadraticHamiltonians","page":"Home","title":"QuadraticHamiltonians","text":"Documentation for QuadraticHamiltonians.\n\n","category":"section"}]
 }
